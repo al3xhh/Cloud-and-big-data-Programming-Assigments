5d278f24412f9914ee63df887a1766cba2fd9e36
Alejandro Huertas Herrero <al3x_hh@eduroam231116.eduroam.ucm.es>
2017-12-20 09:37:34 +0100

    WIP exercise P25.

--- P25_spark.py
+++ P25_spark.py
@@ -27,8 +27,7 @@ overallRatingsRDD = overallRatingsRDD.map(lambda rating: rating.split(";"))
 overallRatingsRDD = overallRatingsRDD.map(lambda userRating: (float(userRating[2])))
 overallRatingsRDD = overallRatingsRDD.sum() / overallRatingsRDD.count()
 print "Overall average"
-print overallRatingsRDD
-
+print overallRatingsRDD'''
 
 moviesRatingsRDD = moviesRatingsRDD.map(lambda rating: rating.split(";"))
 moviesRatingsRDD = moviesRatingsRDD.map(lambda movieRating: (movieRating[1], float(movieRating[2])))
@@ -38,7 +37,7 @@ moviesRatingsRDD = moviesRatingsRDD.sortByKey()
 print "Average rating by movie"
 print moviesRatingsRDD.take(10)
 
-
+'''
 def splitGenres(tuple):
     for genre in tuple[0]:
         return genre, tuple[1]
@@ -53,23 +52,26 @@ genresMoviesRatingsRDD = genresMoviesRatingsRDD.map(splitGenres)
 genresMoviesRatingsRDD = genresMoviesRatingsRDD.groupByKey()
 genresMoviesRatingsRDD = genresMoviesRatingsRDD.map(lambda genreRating: (genreRating[0], sum(list(genreRating[1])) / len(genreRating[1])))
 print "Average rating by genre"
-print genresMoviesRatingsRDD.take(10)
-'''
+print genresMoviesRatingsRDD.take(10)'''
+
 moviesTitlesRDD = moviesTitlesRDD.map(lambda movie: movie.split(";"))
 moviesTitlesRDD = moviesTitlesRDD.map(lambda movie: (movie[0], movie[1]))
 topMoviesRatingsRDD = moviesRatingsRDD.join(moviesTitlesRDD)
-topMoviesRatingsRDD = topMoviesRatingsRDD.map(lambda movieRating: (movieRating[1][0], movieRating[1][1]))
+topMoviesRatingsRDD = topMoviesRatingsRDD.map(lambda movieRating: (movieRating[1][0], movieRating[1][1] + "," + movieRating[0]))
 topMoviesRatingsRDD = topMoviesRatingsRDD.sortByKey(False)
+topMoviesRatingsIdsRDD = topMoviesRatingsRDD
+topMoviesRatingsIdsRDD = topMoviesRatingsIdsRDD.map(lambda movieId: (movieId[1].split(",")[1], movieId[1].split(",")[0]))
 topMoviesRatingsRDD = topMoviesRatingsRDD.map(lambda movieRating: (movieRating[1], movieRating[0]))
 print "Top 10"
 print topMoviesRatingsRDD.take(10)
 
+
 timestampRDD = timestampRDD.map(lambda timestamp: timestamp.split(";"))
 ratingTimestampRDD = timestampRDD.map(lambda timestamp: (timestamp[1], str(timestamp[2]) + ";" + str(datetime.datetime.fromtimestamp(int(timestamp[3])).month)))
-moviesRatingTimestampRDD = ratingTimestampRDD.join(moviesTitlesRDD)
+moviesRatingTimestampRDD = ratingTimestampRDD.join(topMoviesRatingsIdsRDD)
 moviesRatingTimestampRDD = moviesRatingTimestampRDD.map(lambda movie: (movie[1][0].split(";")[1], movie[1][1]))
-moviesRatingTimestampRDD = moviesRatingTimestampRDD.groupByKey()
-moviesRatingTimestampRDD = moviesRatingTimestampRDD.sortByKey()
+#moviesRatingTimestampRDD = moviesRatingTimestampRDD.groupByKey()
+#moviesRatingTimestampRDD = moviesRatingTimestampRDD.sortByKey()
 #movieRatingTimestampRDD = ratingTimestampRDD.join(movieTimestampRDD)
 #movieRatingTimestampRDD = movieTimestampRDD.groupByKey()
 #movieRatingTimestampRDD = movieRatingTimestampRDD.sortByKey()
