0176676b0c4004463b6b4eb52cc76d549191443b
Alejandro Huertas Herrero <al3x_hh@MacBook-Pro-de-Alejandro.local>
2018-01-07 12:55:38 +0100

    Finished exercise P4.

--- P4_spark.py
+++ P4_spark.py
@@ -2,6 +2,7 @@
 
 import sys
 from pyspark import SparkContext
+from random import randint
 
 def is_number(s):
     try:
@@ -29,6 +30,18 @@ def count(word):
     ret.append((last, count))
     return (word[0], ret)
 
+def max(word):
+    wordList = list(word[0][1])
+    maxObject = None
+
+    for w in wordList:
+        if maxObject is None:
+            maxObject = w
+        elif w[1] > maxObject[1]:
+            maxObject = w
+
+    return word[0][0] + " " + maxObject[0]
+
 sc = SparkContext()
 shakespeareRDD = sc.textFile("../Data/P4_data.txt")
 shakespeareRDD = shakespeareRDD.flatMap(lambda word: word.split(" "))
@@ -47,4 +60,11 @@ shakespeareRDD4 = shakespeareRDD4.sortByKey()
 shakespeareRDD4 = shakespeareRDD4.map(lambda word: (word[1][1][0] + " " + word[1][1][1], word[1][0][1]))
 shakespeareRDD4 = shakespeareRDD4.groupByKey()
 shakespeareRDD4 = shakespeareRDD4.map(count)
-print shakespeareRDD4.take(30)
+
+for i in range(0, 10):
+    ret = ""
+
+    for j in range(0, 20):
+        ret += max(shakespeareRDD4.takeSample(True, 1))
+
+    print ret
